5362f6959a38d8ae2da8ca9eeb9f2eab5132e1ad
Mohamed-Alaa <mohamed.alaa.el.din.hassan@gmail.com>
2017-10-23 15:54:50 +0200

    getsircles

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,32 @@
 $(function(){
 'use scrict';
-$('.featured-work ul li').on('click',function() {
-    $(this).addClass('active').siblings().removeClass('active');
+    $('.featured-work ul li').on('click',function() {
+        $(this).addClass('active').siblings().removeClass('active');
 
-if ($(this).data('class') === 'all'){
- $('.mix-move .col-md').css('opacity', 1);
-} else {
+        if ($(this).data('class') === 'all'){
+            $('.mix-move .col-md').css('opacity', 1);
+        } else {
 
-    $('.mix-move .col-md').css('opacity', '.08');
-    $($(this).data('class')).parent().css('opacity',1);
-}
+            $('.mix-move .col-md').css('opacity', '.08');
+            $($(this).data('class')).parent().css('opacity',1);
+        }
+    });
 
-});
+    $('#particles').particleground({
+        minSpeedX: 0.3,
+        maxSpeedX: 1.1,
+        minSpeedY: 0.3,
+        maxSpeedY: 1.1,
+        directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+        directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+        density: 5000, // How many particles will be generated: one particle every n pixels
+        dotColor: '#D6D6D6',
+        lineColor: '#D6D6D6',
+        particleRadius: 5, // Dot size
+        lineWidth: 1,
+        curvedLines: true,
+        proximity: 120, // How close two dots need to be before they join
+        parallax: false
+    });
+    $("body").niceScroll({cursorcolor:"#E91E63",cursorwidth: "8px",cursorborder: "1px solid #444",cursorminheight: 100});
 });
